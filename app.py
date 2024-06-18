0b696d139c7ea7fb8b2a9ff15993db4832fc3b72
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-06-18 21:34:24 +0200

    allow cors globally

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
+from flask_cors import CORS
+
 # from chain import invoke_chain
 from chain_instructor import invoke_chain
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/scout-prompt", methods=["GET", "POST"])
+@cross_origin()
 def scout_prompt():
     if not request.is_json:
         return "Not a valid json!", 400
