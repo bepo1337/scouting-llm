a9b6c1269ec0d2c3db03368e77c570aaadf198f5
marvin.schmohl <marvin.schmohl@gmx.de>
2024-07-20 12:07:55 +0200

    Resolved Merge Issues

--- chain.py
+++ chain.py
@@ -40,7 +40,7 @@ vectorstore = Milvus(
     auto_id=True
 )
 
-retriever = vectorstore.as_retriever(search_kwargs={'k': 10})
+retriever = vectorstore.as_retriever(search_kwargs={'k': 2})
 
 ##### Dont have to edit anything below this to change models
 def format_docs(docs):
